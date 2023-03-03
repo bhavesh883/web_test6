26957c2585e03e9bb6f6cebff572c95b7659071c
Bhavesh Chandel <bhaveshchandel883@gmail.com>
2023-03-03 16:38:31 +0530

    first commit

---
+++ model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+
+
+module.exports = mongoose.model("blogger",new mongoose.Schema({
+
+    title:"string",
+    description:"string",
+    markdown:"string",
+    date:Date,
+    id:String
+  
+ }))
+
